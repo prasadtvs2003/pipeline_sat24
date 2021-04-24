a103f657bed50ae6d78a51799c54045c4c76c6ac
student <prasadtvs2003@gmail.com>
2021-04-24 16:23:14 +0000

    PLP

---
+++ src/main/java/com/caltech/democaltech/DemoCaltechApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.democaltech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCaltechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCaltechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/democaltech/DemoCaltechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.democaltech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCaltechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
